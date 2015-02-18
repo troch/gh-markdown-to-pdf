dac075e7c9b1af1d23b5ba19f12cae4ce1ccaf79
Thomas Roch <thomas.c.roch@gmail.com>
2015-02-18 15:06:28 +0000

    Refactor and add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Convert GitHub markdown files to pdf
+
+1. Clone this repository
+2. Run `npm install`
+3. Place your .md files in a folder called `docs` (inside repository)
+4. Run `node index.js`
+5. PDF files are inside the pdf folder!
--- index.js
+++ index.js
@@ -1,105 +1,11 @@
-var fs = require('vinyl-fs'),
-    map = require('map-stream');
 
-var pdf = require('html-pdf'),
-    nunjucks = require('nunjucks'),
-    marked = require('marked');
+var lib = require('./lib');
 
-marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true
-});
-
-// var templateHtml = fs.readFileSync('templates/index.html', 'utf8');
-
-fs
-    .src('./docs/*.md')
-    ////////////////
-    // MD to HTML //
-    ////////////////
-    .pipe(map(function (file, cb) {
-        file.contents = new Buffer(marked(file.contents.toString()));
-        cb(null, file);
-    }))
-    //////////////////
-    // HTML TO PAGE //
-    //////////////////
-    .pipe(map(function (file, cb) {
-        nunjucks.render('./templates/index.html', {
-            cssFile: 'node_modules/github-markdown-css/github-markdown.css',
-            html:  file.contents.toString()
-        }, function(err, res) {
-            if (err) {
-                cb(err);
-            } else {
-                file.contents = new Buffer(res);
-                cb(null, file);
-            }
-        });
-    }))
-    /////////////////
-    // PAGE TO PDF //
-    /////////////////
-    .pipe(map(function (file, cb) {
-        pdf.create(file.contents.toString()).toBuffer(function (err, res) {
-            if (err) {
-                cb(err);
-            } else {
-                file.path = file.path.replace(/\.md$/, '.pdf');
-                file.contents = res;
-                cb(null, file);
-            }
-        });
-    }))
+lib.convertToPdf()
     .on('error', function (err) {
         console.log(err);
         process.exit(1);
     })
-    .pipe(fs.dest('./pdf'))
     .on('end', function () {
         process.exit();
     });
-
-
-   // Q
-   //  .nfcall(fs.readdir, 'docs')
-   //  .then(function (fileNames) {
-   //      ////////////////
-   //      // Read files //
-   //      ////////////////
-   //      return Q.all(
-   //          fileNames.map(function (fileName) {
-   //              return Q.nfcall(fs.readFile, './docs/' + fileName);
-   //          })
-   //      );
-   //  })
-   //  .then(function (files) {
-   //      ////////////////////
-   //      // Template files //
-   //      ////////////////////
-   //      return Q.all(
-   //          files.map(function (file) {
-   //              return Q.nfcall(nunjucks.renderString, templateHtml, {
-   //                  cssFile: 'node_modules/github-markdown-css/github-markdown.css',
-   //                  html:  marked(file.toString())
-   //              });
-   //          })
-   //      );
-   //  })
-   //  .then(function (htmlFiles) {
-   //      ///////////////
-   //      // Write PDF //
-   //      ///////////////
-   //      return Q.all(
-   //          htmlFiles.map(function (htmlFile) {
-   //              var p = pdf.create(htmlFile);
-   //              console.log(p.toFile);
-   //              return Q.nfcall(p.toFile, './pdf/a.pdf');
-   //          })
-   //      );
-   //  })
-   //  .then(process.exit)
-   //  .catch(function (err) {
-   //      console.log(err);
-   //      process.exit();
-   //  });
